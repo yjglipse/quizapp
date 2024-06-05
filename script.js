ba5f22c5341b3818627ca31236802eaafe3c5250
kathelin monique <kathelin.silva@eaportal.org>
2024-06-05 23:32:27 +0000

    adicionada a função para escolher o tema do index.html

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ botaoTema.addEventListener("click", () => {
 verificarTema()
 
 verificarTema(body, botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
