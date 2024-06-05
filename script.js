ceb8dab1fa88b3b45c2df6a4d7ef38c0b27c4291
kathelin monique <kathelin.silva@eaportal.org>
2024-06-05 23:14:38 +0000

    ciração de um helper responsável por mudar o tema

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
 
 const body = document.querySelector("body")
 
-function trocarTema() {
-    if(localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if(localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if(tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
 
 verificarTema()
+
+verificarTema(body, botaoTema)
