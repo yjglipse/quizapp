47faf8055377ecc035fda386b0ed056f874d91b4
kathelin monique <kathelin.silva@eaportal.org>
2024-06-05 23:24:08 +0000

    adição do javascript do quiz.html para trocar o tema

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,10 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+verificarTema(body, botaoTema)
