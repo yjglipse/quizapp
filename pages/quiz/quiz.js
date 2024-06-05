64c939d852ab0703015f8e70728e7183af166e99
kathelin monique <kathelin.silva@eaportal.org>
2024-06-05 23:45:30 +0000

    alteração dinâmica do assunto da página

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,3 +8,18 @@ botaoTema.addEventListener("click", () => {
 })
 
 verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ìcone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
