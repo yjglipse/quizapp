dff41417d88099d2e9d093d6aa3b1a3bac51e6ff
kathelin monique <kathelin.silva@eaportal.org>
2024-06-06 01:26:39 +0000

    up

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
-botaoTema.addEventListener("click", () => {
+botaoTema.addEventListener("click", () =>{
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
 
 const botoesAssunto = document.querySelectorAll(".assuntos button")
-botoesAssunto.forEach(botao => {
+botoesAssunto.forEach(botao =>{
     botao.addEventListener("click", selecionarAssunto)
 })
 
-function selecionarAssunto(evento) {
+function selecionarAssunto(evento){
     const classeBotao = evento.target.className
     const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
